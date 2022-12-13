bd31a7b0678bc0c4796a2e3fc618473a9aa7362f
Sam <github@samscherf.com>
2022-12-13 12:40:10 -0800

    Made code more modular

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
-# Create Standard Directories
-mkdir Documents
-mkdir Projects
+# Create Directories I like
+FOLDERS="Projects Documents"
+for FOLDER in $(echo $FOLDERS)
+do
+	# If directory exists
+	[ -d "$FOLDER" ] \
+	&& (echo "$FOLDER folder already exists") \
+	|| (mkdir $FOLDER; echo "Created $FOLDER folder")
+done
